ea1e8d6d5ae70bf16d7f28fd01abc599d9ecfd18
Kieron Wilkinson <kieron.wilkinson@gmail.com>
2018-09-29 09:42:02 +0100

    Thanks Sergey!

--- server/src/test/kotlin/com/github/vyadh/teamcity/keyvault/server/KotlinMockitoMatchers.kt
+++ server/src/test/kotlin/com/github/vyadh/teamcity/keyvault/server/KotlinMockitoMatchers.kt
@@ -2,6 +2,10 @@ package com.github.vyadh.teamcity.keyvault.server
 
 import org.mockito.Mockito
 
+/**
+ * Mockito-Kotlin compatibility class, a hat-tip to Sergey:
+ * https://stackoverflow.com/questions/30305217/is-it-possible-to-use-mockito-in-kotlin
+ */
 object KotlinMockitoMatchers {
 
   fun <T> any(): T {
@@ -9,6 +13,7 @@ object KotlinMockitoMatchers {
     return uninitialized()
   }
 
+  @Suppress("UNCHECKED_CAST")
   private fun <T> uninitialized(): T = null as T
 
 }

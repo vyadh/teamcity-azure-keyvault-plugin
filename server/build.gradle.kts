a9fc958ab2e912762fa2bd7bb7b6f036a8db665f
Kieron Wilkinson <kieron.wilkinson@gmail.com>
2018-11-25 00:37:35 +0000

    Fixed a problem including full Kotlin compiler in plugin package because of misconfigured Moshi dependency

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -30,8 +30,10 @@ dependencies {
 
   compile("com.squareup.okhttp3:okhttp:3.11.0")
   testImplementation("com.squareup.okhttp3:mockwebserver:3.11.0")
-  compile("com.squareup.moshi:moshi-kotlin-codegen:1.6.0")
-  kapt("com.squareup.moshi:moshi-kotlin-codegen:1.6.0")
+
+  compile("com.squareup.moshi:moshi:1.8.0")
+  compileOnly("com.squareup.moshi:moshi-kotlin-codegen:1.8.0")
+  kapt("com.squareup.moshi:moshi-kotlin-codegen:1.8.0")
 
   testCompile("org.assertj:assertj-core:3.11.1")
   testCompile("org.junit.jupiter:junit-jupiter-api:5.3.1")
